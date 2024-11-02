65a536b49e8af27cab2987d791a8b6b893b64d3d
Nolan332933 <nolansehlby81@gmail.com>
2024-11-02 11:51:28 +0630

    departure and destinatin ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,16 @@
 import Header from "@/components/Header";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { ActivityIndicator, Pressable, View, Text } from "react-native";
 import { ArrowPathRoundedSquareIcon, ChevronDoubleRightIcon } from "react-native-heroicons/outline";
+
+interface SearchFlightData {
+  originCity: string;
+  destinationCity: string;
+  departureDate: string;
+  seat: number;
+}
 
 interface TripOptionProps {
   pageNavigation: string;
@@ -72,9 +80,93 @@ const TripOption: React.FC<TripOptionProps> = ({
   </View>
 );
 
+interface locationInputProps {
+  placeholder: string;
+  icon: React.ReactNode;
+  value: string;
+  onPress: () => void;
+}
+
+const LocationInput: React.FC<locationInputProps> = ({
+  placeholder,
+  icon,
+  value,
+  onPress
+}) => (
+  <View>
+    <Pressable className="flex-row justify-between w-full px-4 py-2" onPress={onPress}>
+      <View className="border-2 border-gray-300 mx-4 mb-4 rounded-2xl justify-center">
+        <Pressable onPress={onPress}>
+          <View className="px-4 flex-row justify-between items-center">
+              <View className="w-[15%] border-r-2 border-gray-200">
+                 {icon}
+              </View>
+              <View className="w-[80%] py-3">
+                {
+                  value ? (
+                    <Text className="bg-transparent text-gray-600 font-bold">
+                      {value}
+                    </Text>
+                  ) : 
+                  (
+                    <Text className="bg-transparent text-lg text-gray-600 font-semibold">
+                      {placeholder}
+                    </Text>
+                  )
+                }
+              </View>
+          </View>
+        </Pressable>
+      </View>
+    </Pressable>
+  </View>
+)
+
+interface DepartureDateProps {
+  placeholder: string;
+  icon: React.ReactNode;
+  value: string;
+  onPress: () => void;
+}
+
+const DepartureDate: React.FC<DepartureDateProps> = ({
+  placeholder,
+  icon,
+  value,
+  onPress,
+}) => {
+  return (
+    <Pressable className="flex-row justify-between w-full px-4 py-2" onPress={onPress}>
+    <View className="border-2 border-gray-300 mx-4 mb-4 rounded-2xl justify-center">
+      <Pressable onPress={onPress}>
+        <View className="px-4 flex-row justify-between items-center">
+          <View className="w-[15%] border-r-2 border-gray-300">{icon}</View>
+          <View className="w-[80%] py-3">
+            {value ? (
+              <Text className="bg-transparent text-gray-600 font-bold">{value}</Text>
+            ) : (
+              <Text className="bg-transparent text-lg text-gray-600 font-semibold">
+                {placeholder}
+              </Text>
+            )}
+          </View>
+        </View>
+      </Pressable>
+    </View>
+    </Pressable>
+  );
+};
+
+
 export default function HomeScreen() {
   const [isPending, setIsPending] = useState(false);
   const [pageNavigation, setPageNavigation] = useState("oneWay");
+  const [searchFlightData, setSearchFlightData] = useState<SearchFlightData>({
+    originCity: "",
+    destinationCity: "",
+    departureDate: "",
+    seat: 0,
+  })
 
   const handleNavigationChange = (type: string) => setPageNavigation(type);
   return (
@@ -104,9 +196,38 @@ export default function HomeScreen() {
           <View className="flex-row justify-between w-full px-4 py-2">
             <TripOption
               pageNavigation={pageNavigation}
-              handleNavigationChange={(type) => setPageNavigation(type)}
+              handleNavigationChange={handleNavigationChange}
             />
           </View>
+          <LocationInput
+            placeholder={
+              searchFlightData.originCity ? searchFlightData.originCity : "Departure City"
+            }
+            icon={<FontAwesome5 size={20} color="gray" name="plane-departure"/>}
+            value={searchFlightData.originCity}
+            onPress={() => {}}
+          />
+
+          <LocationInput
+            placeholder={
+              searchFlightData.originCity ? searchFlightData.originCity : "Destination City"
+            }
+            icon={<FontAwesome5 size={20} color="gray" name="plane-arrival"/>}
+            value={searchFlightData.originCity}
+            onPress={() => {}}
+          />
+
+          <DepartureDate
+             placeholder={
+              searchFlightData.departureDate ?
+               searchFlightData.departureDate
+               : "Departure Date"
+             }
+             icon={<FontAwesome5 size={20} color="gray" name="calendar-alt"/>}
+             value={searchFlightData.departureDate}
+             onPress={()=> {}}
+          />
+
         </View>
       </View>
     </View>
